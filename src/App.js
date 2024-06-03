b07461db90591ead757f4636e607da63e468880c
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-03 21:28:26 +0530

    feat: install and configure React-Router-DOM with createBrowserRouter and Outlet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { Footer, Navbar } from "./components";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BookSearchPage, MyBookshelf } from "./pages";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <BookSearchPage /> },
+      { path: "my-bookshelf", element: <MyBookshelf /> },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
