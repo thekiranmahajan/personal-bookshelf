7e4bff56f167181d06bea764c8e548b86b389140
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-04 11:13:40 +0530

    fix: NaN error when undefined present in API response by providing OR values + minor styling

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,29 +1,38 @@
 import React from "react";
+import Button from "./Button";
 
 const BookCard = ({
   book: { title, author_name, publisher, publish_year, edition_count },
 }) => {
   return (
-    <div className="mt-2 flex w-72 flex-col items-start justify-center rounded-lg bg-[#AEF78E] px-3 py-4">
-      <h2 className="font-bold">
-        Book Title: <span className="text-sm text-gray-600">{title}</span>
-      </h2>
-      <h2 className="font-bold">
-        Author Name:
-        <span className="text-sm text-gray-600">{author_name?.join(", ")}</span>
-      </h2>
-      <h2 className="font-bold">
-        Publisher:
-        <span className="text-sm text-gray-600">
-          {publisher[publisher?.length - 1]}(
-          {publish_year[publish_year?.length - 1]})
+    <div className="relative m-3 flex min-h-52 w-72 flex-col items-start justify-center rounded-lg bg-[#AEF78E] px-3 py-4 pb-7 font-bold text-gray-800 shadow-lg transition-all duration-300 hover:scale-105">
+      <h2>
+        Book Title:{" "}
+        <span className="text-sm font-medium text-gray-600">
+          {title || "N/A"}
         </span>
       </h2>
-      <h2 className="font-bold">
-        Edition Count:
-        <span className="text-sm text-gray-600">{edition_count}</span>
+      <h2>
+        Author Name:{" "}
+        <span className="text-sm font-medium text-gray-600">
+          {author_name ? author_name.join(", ") : "N/A"}
+        </span>
       </h2>
-      <button className="bg-[#66A182]">Add to Bookshelf</button>
+      <h2>
+        Publisher:{" "}
+        <span className="text-sm font-medium text-gray-600">
+          {publisher && publish_year
+            ? `${publisher[publisher.length - 1]} (${publish_year[publish_year.length - 1]})`
+            : "N/A"}
+        </span>
+      </h2>
+      <h2>
+        Edition Count:{" "}
+        <span className="text-sm font-medium text-gray-600">
+          {edition_count || "N/A"}
+        </span>
+      </h2>
+      <Button btnText={"Add to Bookshelf"} />
     </div>
   );
 };
